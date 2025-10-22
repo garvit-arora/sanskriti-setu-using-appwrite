62539927ba3e8cb0864e059dba4a8283f3a02baf
Garvit Arora <garvit.university@gmail.com>
2025-10-22 11:20:10 +0530

    updating

---
+++ Desktop/Web Dev/Projects/AutoRat/autorat/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+const Page = ()=>{
+  return(
+    // <div className="text-red-500">
+    //   Hello World
+    // </div>
+  <div className="min-h-screen min-w-screen flex items-center justify-center ">
+    <Button className="cursor-pointer" >
+    Click Me
+    </Button>
+  </div>
+  )
+};
+export default Page;
